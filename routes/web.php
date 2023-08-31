2eb278b4646e6f1957c3f07723ad269c9d6ff267
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-08-31 22:50:57 +0900

    記事表示ページの基盤

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\FavoritecategoryController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\GoodController;
-use App\Http\Controllers\RoomController;
-use App\Http\Controllers\MessageController;
-use App\Http\Controllers\EntryController;
-use App\Http\Controllers\RelationshipController;
-use App\Http\Controllers\IconController;
-use App\Http\Controllers\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,21 +13,4 @@ use App\Http\Controllers\ImageController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/category', [CategoryController::class, 'category']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/article', [ArticleController::class, 'article']);
-Route::get('/profile', [ProfileController::class, 'profile']);
-Route::get('/favoritecategory', [FavoritecategoryController::class, 'favoritecategory']);
-Route::get('/comment', [CommentController::class, 'comment']);
-Route::get('/good', [GoodController::class, 'good']);
-Route::get('/room', [RoomController::class, 'room']);
-Route::get('/message', [MessageController::class, 'message']);
-Route::get('/entry', [EntryController::class, 'entry']);
-Route::get('/relationship', [RelationshipController::class, 'relationship']);
-Route::get('/icon', [IconController::class, 'icon']);
-Route::get('/image', [ImageController::class, 'image']);
+Route::get('/', [ArticleController::class, 'article']);
