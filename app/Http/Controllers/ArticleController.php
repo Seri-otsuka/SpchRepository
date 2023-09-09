6c9da529a601eb0a3c87062b7739cbd21f7552ba
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-09-09 10:20:44 +0900

    追加

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+use App\Http\Requests\ArticleRequest;
+
+class ArticleController extends Controller
+{
+
+    //記事表示一覧のメソッド
+    public function article(Article $article)
+    {
+        return view('articles.index')->with(['articles' => $article->getPaginateByLimit()]);
+    }
+    //記事詳細表示のメソッド
+    public function show(Article $article)
+    {
+        return view('articles.show')->with(['article' => $article]);
+    }
+    //記事作成画面のメソッド
+    public function create(Article $article)
+    {
+        return view('articles.create');
+    }
+    //記事保存のメソッド
+    public function store(Article $article, ArticleRequest $request)
+    {
+        $input = $request['article'];
+        $article->fill($input)->save();
+        return redirect('/articles/' . $article->id);
+    }
+
+    //記事編集のメソッド
+    public function edit(Article $article)
+    {
+        return view('articles.edit')->with(['article' => $article]);
+    }
+
+    //記事編集後投稿のメソッド
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $input_article = $request['article'];
+        $article->fill($input_article)->save();
+
+        return redirect('/articles/' . $article->id);
+    }
+}
