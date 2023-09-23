baf366d16a023c79bee2a03fe4c7afd4d12961ac
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-09-24 01:44:14 +0900

    css3回目

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,8 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <!--背景の色-->
+        <div class="min-h-screen bg-red-100">
             @include('layouts.navigation')
 
             <!-- Page Heading --><!--ヘッダーのこと-->
