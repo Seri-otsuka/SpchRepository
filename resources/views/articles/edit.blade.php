668850c3112b0bebdcaa05fffc9c245581f08845
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-09-24 19:06:55 +0900

    css(主にヘッダー)

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,28 +1,43 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-    </head>
-    <body>
-        <h1>BlogName</h1>
-        <form action="/articles/{{ $article->id }}" method="POST">
-            @csrf
-            @method('PUT')
-            <div class="title">
-                <h2>Title</h2>
-                <input type="text" name="article[title]" placeholder="タイトル" value="{{ $article->title }}" />
-                <p class="title_error" style="color:red">{{ $errors->first('article.title') }}</p>
+<x-app-layout>
+    <!DOCTYPE html>
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <head>
+            <meta charset="utf-8">
+            <title>Blog</title>
+        </head>
+        <body>
+            　<x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('記事を編集する') }}
+                </h2>
+            </x-slot>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900">
+                            <form action="/articles/{{ $article->id }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                <div class="title">
+                                    <h2>Title</h2>
+                                    <input class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" type="text" name="article[title]" placeholder="タイトル" value="{{ $article->title }}" />
+                                    <p class="title_error" style="color:red">{{ $errors->first('article.title') }}</p>
+                                </div>
+                                <div class="text">
+                                    <h2>text</h2>
+                                    <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm" name="article[text]" placeholder="ここに記事を書いてね！">{{ $article->text }}</textarea>
+                                    <p class="text_error" style="color:red">{{ $errors->first('article.text') }}</p>
+                                </div>
+                                
+                                <input class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" type="submit" value="投稿" />
+                            </form>
+                            <div class="footer">
+                                <a href="/articles/{{ $article->id}}">戻る</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="text">
-                <h2>text</h2>
-                <textarea name="article[text]" placeholder="ここに記事を書いてね！">{{ $article->text }}</textarea>
-                <p class="text_error" style="color:red">{{ $errors->first('article.text') }}</p>
-            </div>
-            <input type="submit" value="投稿" />
-        </form>
-        <div class="footer">
-            <a href="/articles/{{ $article->id}}">戻る</a>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
+</x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- ログインのレイアウト -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('article') }}">
-                        <h1>すぷちゃ</h1>
+                        <h1 class="text-white text-xl">すぷちゃ</h1>
                     </a>
                 </div>
 
@@ -22,28 +22,23 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('introduction')" :active="request()->routeIs('introduction')">
+                         <a href="/categories/1" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                         {{ __('アニメ') }}
-                    </x-nav-link>
+                    </a>
                 </div>
                  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('introduction')" :active="request()->routeIs('introduction')">
                         {{ __('ゲーム') }}
                     </x-nav-link>
                 </div>
-                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('introduction')" :active="request()->routeIs('introduction')">
+                  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                         <a href="/categories/2" class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                         {{ __('K-POP') }}
-                    </x-nav-link>
+                    </a>
                 </div>
                  <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('introduction')" :active="request()->routeIs('introduction')">
                         {{ __('サイトについて') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('ダッシュボード') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -51,12 +46,11 @@
             
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                  <img class="w-10 h-10 rounded-full object-cover border-none bg-gray-200" src="{{ isset(Auth::user()->profile_photo_path) ? asset('storage/' . Auth::user()->profile_photo_path) : asset('images/user_icon.png') }}">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                            <img class="w-10 h-10 rounded-full object-cover border-none bg-gray-200" src="{{ isset(Auth::user()->profile_photo_path) ? asset('storage/' . Auth::user()->profile_photo_path) : asset('images/user_icon.png') }}">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
